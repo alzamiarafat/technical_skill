4dbe76beb50c2febba60c311d82f51b52091a2a7
alzamiarfat <alzamiarfat00@gmail.com>
2022-10-12 09:17:27 +0600

    feat: user, outlet crud api develop

---
+++ app/Http/Controllers/Api/OutletController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Outlet;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class OutletController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $outlets = Outlet::all();
+        return response()->json($outlets);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $outlet = Outlet::where('id', $id)->first();
+        return response()->json($outlet);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Outlet::find($id)->delete();
+        return response()->json("User has been deleted successfully!");
+    }
+}
---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return
+     */
+    public function index()
+    {
+        $users = User::all();
+        return UserResource::collection($users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return
+     */
+    public function store(UserRequest $request)
+    {
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        return  new UserResource($user);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return
+     */
+    public function show($id)
+    {
+        $user = User::where('id', $id)->first();
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return
+     */
+    public function update(UserRequest $request, $id)
+    {
+        $user = User::where('id', $id)->first();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response(null, 204);
+    }
+}
